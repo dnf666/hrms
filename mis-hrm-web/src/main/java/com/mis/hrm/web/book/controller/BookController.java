188f7fef0fc4a852961bd4a719d5720c88d4de0d
123liudong <1911372737@qq.com>
2018-08-12 10:59:01 +0800

    移动book和project模块的controller层到web模块去，并且讲apidoc的配置移动到web模块

--- mis-hrm-web/src/main/java/com/mis/hrm/web/book/controller/BookController.java
+++ mis-hrm-web/src/main/java/com/mis/hrm/web/book/controller/BookController.java
@@ -1,16 +1,197 @@
 package com.mis.hrm.web.book.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.Resource;
+import com.mis.hrm.book.po.Book;
+import com.mis.hrm.book.util.ConstantValue;
+import com.mis.hrm.util.ToMap;
 
-/**
- * created by dailf on 2018/7/13
- *
- * @author dailf
- */
-@Controller
+import java.util.LinkedList;
+import java.util.Map;
+
 public class BookController {
+    /**
+     *   @api {GET} book-list-1 通过公司id
+     *   @apiDescription 通过公司id得到书录
+     *   @apiGroup BOOK-QUERY
+     *   @apiParam  {String} companyId 公司id
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     * 	            "bookId": "dddddfffffsssss",
+     * 	            "bookName": "你的灯亮着吗？",
+     * 	            "category": "思维",
+     * 	            "companyId": "信管工作室",
+     * 	            "quantity": 2,
+     * 	            "version": "2.0"
+     *              }, {
+     * 	            "bookId": "reerrrrrrr",
+     * 	            "bookName": "你的灯亮着吗？",
+     *          	"category": "思维",
+     *          	"companyId": "信管工作室",
+     *           	"quantity": 2,
+     *           	"version": "2.0"
+     *              }]
+     *              }
+     */
+    public Map getBooksByCompanyId(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,new LinkedList<Book>());
+    }
 
+    /**
+     *   @api {GET} book-list-2 通过公司id和类别
+     *   @apiDescription 通过公司id和分类得到书录
+     *   @apiGroup BOOK-QUERY
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} category 分类
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     * 	            "bookId": "dddddfffffsssss",
+     * 	            "bookName": "你的灯亮着吗？",
+     * 	            "category": "思维",
+     * 	            "companyId": "信管工作室",
+     * 	            "quantity": 2,
+     * 	            "version": "2.0"
+     *              }, {
+     * 	            "bookId": "reerrrrrrr",
+     * 	            "bookName": "你的灯亮着吗？",
+     *          	"category": "思维",
+     *          	"companyId": "信管工作室",
+     *           	"quantity": 2,
+     *           	"version": "2.0"
+     *              }]
+     *              }
+     */
+    public Map getBooksByCompanyAndCateGory(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,new LinkedList<Book>());
+    }
+
+    /**
+     *   @api {GET} book-list-3 通过公司id和书名
+     *   @apiDescription 通过公司id和书名得到书录
+     *   @apiGroup BOOK-QUERY
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} bookName 书的名称
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     * 	            "bookId": "dddddfffffsssss",
+     * 	            "bookName": "你的灯亮着吗？",
+     * 	            "category": "思维",
+     * 	            "companyId": "信管工作室",
+     * 	            "quantity": 2,
+     * 	            "version": "2.0"
+     *              }, {
+     * 	            "bookId": "reerrrrrrr",
+     * 	            "bookName": "你的灯亮着吗？",
+     *          	"category": "思维",
+     *          	"companyId": "信管工作室",
+     *           	"quantity": 2,
+     *           	"version": "2.0"
+     *              }]
+     *              }
+     */
+    public Map getBooksByCompanyAndBookName(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,new LinkedList<Book>());
+    }
+
+    /**
+     *   @api {GET} book-list-4 通过书的id
+     *   @apiDescription 通过书的id得到书的信息
+     *   @apiGroup BOOK-QUERY
+     *   @apiParam  {String} bookId 书的id
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":{
+     * 	            "bookId": "dddddfffffsssss",
+     * 	            "bookName": "你的灯亮着吗？",
+     * 	            "category": "思维",
+     * 	            "companyId": "信管工作室",
+     * 	            "quantity": 2,
+     * 	            "version": "2.0"
+     *              }
+     *       }
+     */
+    public Map getBookByBookId(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,Book.builder().build());
+    }
+
+    /**
+     *   @api {PUT} book 通过书的id更新书的信息
+     *   @apiDescription 通过书的id更新书的信息，同时返回更新后的信息
+     *   @apiGroup BOOK-UPDATE
+     *   @apiParam  {String} bookId 书的id
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} bookName 书名
+     *   @apiParam  {String} category 类别
+     *   @apiParam  {String} quantity 数量
+     *   @apiParam  {String} version 版本
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":{
+     * 	            "bookId": "dddddfffffsssss",
+     * 	            "bookName": "你的灯亮着吗？",
+     * 	            "category": "思维",
+     * 	            "companyId": "信管工作室",
+     * 	            "quantity": 2,
+     * 	            "version": "2.0"
+     *              }
+     *       }
+     */
+    public Map updateBookByBookId(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,Book.builder().build());
+    }
+
+    /**
+     *   @api {POST} book 插入一书本的信息
+     *   @apiDescription 插入一本书的信息
+     *   @apiGroup BOOK-ADD
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} bookName 书名
+     *   @apiParam  {String} category 类别
+     *   @apiParam  {String} quantity 数量
+     *   @apiParam  {String} version 版本
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object": null
+     *       }
+     */
+    public Map addBookInfo(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,null);
+    }
+
+    /**
+     *   @api {DELETE} book 通过bookId
+     *   @apiDescription 通过bookId删除一本书的信息
+     *   @apiGroup BOOK-DELETE
+     *   @apiParam  {String} bookId 书的id
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object": null
+     *       }
+     */
+    public Map deleteBookInfoByBookId(Book book){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS,null);
+    }
 }
---
+++ mis-hrm-web/src/main/java/com/mis/hrm/web/book/controller/BookLendController.java
@@ -0,0 +1,233 @@
+package com.mis.hrm.web.book.controller;
+
+import com.mis.hrm.book.po.BookLendInfo;
+import com.mis.hrm.book.util.ConstantValue;
+import com.mis.hrm.util.ToMap;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public class BookLendController {
+    /**
+     *   @api {GET} booklend-list-1 通过借书者
+     *   @apiDescription 通过借书者查询借书信息
+     *   @apiGroup BOOKLEND-QUERY
+     *   @apiParam  {String} borrower 借书者
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     *     	   "bookName": "暗时间",
+     * 	       "bookRecord": "不晓得是撒子啊",
+     * 	       "borrower": "优秀的人",
+     * 	       "companyId": "信管工作室",
+     * 	       "lendTime": "2018-08-08",
+     * 	       "returnTime": "2018-08-08"
+     *          },{
+     * 	        "bookName": "暗时间",
+     * 	        "bookRecord": "不晓得是撒子啊",
+     * 	        "borrower": "优秀的人",
+     * 	        "companyId": "信管工作室",
+     * 	        "lendTime": "2018-08-08",
+     * 	        "returnTime": "2018-08-08"
+     *         }]
+     *
+     *       }
+     */
+    public Map getBookLendInfosByBorrower(String borrower){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, new LinkedList<BookLendInfo>());
+    }
+
+    /**
+     *   @api {GET} booklend-list-2 通过公司id查询借书的信息
+     *   @apiDescription 通过公司id查询借书信息
+     *   @apiGroup BOOKLEND-QUERY
+     *   @apiParam  {String} companyId 公司id
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     *     	   "bookName": "暗时间",
+     * 	       "bookRecord": "不晓得是撒子啊",
+     * 	       "borrower": "优秀的人",
+     * 	       "companyId": "信管工作室",
+     * 	       "lendTime": "2018-08-08",
+     * 	       "returnTime": "2018-08-08"
+     *          },{
+     * 	        "bookName": "暗时间",
+     * 	        "bookRecord": "不晓得是撒子啊",
+     * 	        "borrower": "优秀的人",
+     * 	        "companyId": "信管工作室",
+     * 	        "lendTime": "2018-08-08",
+     * 	        "returnTime": "2018-08-08"
+     *         }]
+     *
+     *       }
+     */
+    public Map getBookLendInfosByCompanyId(String companyId){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, new LinkedList<BookLendInfo>());
+    }
+
+    /**
+     *   @api {GET} booklend-list-3 通过公司和书名
+     *   @apiDescription 通过公司和书名查询借书信息
+     *   @apiGroup BOOKLEND-QUERY
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} borrower 借书者
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     *     	   "bookName": "暗时间",
+     * 	       "bookRecord": "不晓得是撒子啊",
+     * 	       "borrower": "优秀的人",
+     * 	       "companyId": "信管工作室",
+     * 	       "lendTime": "2018-08-08",
+     * 	       "returnTime": "2018-08-08"
+     *          },{
+     * 	        "bookName": "暗时间",
+     * 	        "bookRecord": "不晓得是撒子啊",
+     * 	        "borrower": "优秀的人",
+     * 	        "companyId": "信管工作室",
+     * 	        "lendTime": "2018-08-08",
+     * 	        "returnTime": "2018-08-08"
+     *         }]
+     *
+     *       }
+     */
+    public Map getBookLendInfosByCompanyIdAndBookName(String companyId, String bookName){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, new LinkedList<BookLendInfo>());
+    }
+
+    /**
+     *   @api {GET} booklend-list-4 通过公司名和借书的记录
+     *   @apiDescription 通过公司名和书的记录查询借书信息
+     *   @apiGroup BOOKLEND-QUERY
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} bookRecord 借书记录
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":{
+     *     	   "bookName": "暗时间",
+     * 	       "bookRecord": "不晓得是撒子啊",
+     * 	       "borrower": "优秀的人",
+     * 	       "companyId": "信管工作室",
+     * 	       "lendTime": "2018-08-08",
+     * 	       "returnTime": "2018-08-08"
+     *          }
+     *       }
+     */
+    public Map getBookLendInfosByCompanyIdAndBookRecord(BookLendInfo bookLendInfo){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, BookLendInfo.builder().build());
+    }
+
+    /**
+     *   @api {GET} booklend-list-５ 得到所有借书信息
+     *   @apiDescription 得到所有的借书信息
+     *   @apiGroup BOOKLEND-QUERY
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":[{
+     *     	   "bookName": "暗时间",
+     * 	       "bookRecord": "不晓得是撒子啊",
+     * 	       "borrower": "优秀的人",
+     * 	       "companyId": "信管工作室",
+     * 	       "lendTime": "2018-08-08",
+     * 	       "returnTime": "2018-08-08"
+     *          },{
+     * 	        "bookName": "暗时间",
+     * 	        "bookRecord": "不晓得是撒子啊",
+     * 	        "borrower": "优秀的人",
+     * 	        "companyId": "信管工作室",
+     * 	        "lendTime": "2018-08-08",
+     * 	        "returnTime": "2018-08-08"
+     *         }]
+     *
+     *       }
+     */
+    public Map getBookLendInfos(){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, new LinkedList<BookLendInfo>());
+    }
+
+
+    /**
+     *   @api {POST} bookLendInfo 插入借一本书的信息
+     *   @apiDescription 插入一本借书的信息
+     *   @apiGroup BOOKLEND-ADD
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam  {String} bookName 书名
+     *   @apiParam  {String} lendTime 借书时间
+     *   @apiParam  {String} returnTime 归还时间（可以为空）
+     *   @apiParam  {String} borrower 借书者
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object": null
+     *       }
+     */
+    public Map insertBookLendInfo(BookLendInfo bookLendInfo){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, null);
+    }
+
+
+    /**
+     *   @api {DELETE} bookLendInfo 通过companyId & bookRecord 删除借书信息
+     *   @apiDescription 通过companyId & bookRecord 删除借书信息
+     *   @apiGroup BOOKLEND-DELETE
+     *   @apiParam  {String} companyId 公司的id
+     *   @apiParam  {String} bookRecord 借书记录
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object": null
+     *       }
+     */
+    public Map deleteBookLendInfo(BookLendInfo bookLendInfo){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, null);
+    }
+
+    /**
+     *   @api {PUT} bookLendInfo 通过companyId & bookRecord 更改借书信息
+     *   @apiDescription 通过companyId & bookRecord 更改借书信息，同时返回更新后的信息
+     *   @apiGroup BOOKLEND-UPDATE
+     *   @apiParam  {String} companyId 公司id
+     *   @apiParam {String} bookRecord 借书记录
+     *   @apiParam  {String} bookName 书名
+     *   @apiParam  {String} lendTime 借书时间
+     *   @apiParam  {String} returnTime 归还时间
+     *   @apiParam  {String} borrower 借书者
+     *   @apiSuccessExample {json} Success-Response:
+     *       HTTP/1.1 200 OK
+     *       {
+     *         "code": "1",
+     *         "msg": "success"
+     *         "object":{
+     *          	   "bookName": "暗时间",
+     *       	       "bookRecord": "不晓得是撒子啊",
+     *       	       "borrower": "优秀的人",
+     *       	       "companyId": "信管工作室",
+     *       	       "lendTime": "2018-08-08",
+     *       	       "returnTime": "2018-08-08"
+     *               }
+     *       }
+     */
+    public Map updateBookLendInfo(BookLendInfo bookLendInfo){
+        return ToMap.toMap(ConstantValue.SUCCESS_CODE, ConstantValue.SUCCESS, null);
+    }
+}
