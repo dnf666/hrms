7de12390f83c6f886156512d0eaeb70adb3b6bb9
yuanyiwen <yuan15826770919@126.com>
2018-08-12 11:26:11 +0800

    又双叒叕没合并完

--- mis-hrm-web/src/test/java/com/mis/hrm/web/book/controller/BookControllerTest.java
+++ mis-hrm-web/src/test/java/com/mis/hrm/web/book/controller/BookControllerTest.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.*;
 @ContextConfiguration("classpath:spring/spring-web.xml")
 @Slf4j
 public class BookControllerTest {
-<<<<<<< HEAD
     @Resource
     private BookService bookService;
     @Test
@@ -37,7 +36,4 @@ public class BookControllerTest {
         Book demo = Book.builder().build();
         log.info(demo.toString());
     }
-=======
-
->>>>>>> 188f7fef0fc4a852961bd4a719d5720c88d4de0d
 }
